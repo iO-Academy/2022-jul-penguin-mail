95fe5349f120975bf648052676ffbbb56a0a7345
Josh Bennet <joshbennet0@gmail.com>
2022-08-30 15:42:46 +0100

    working out how to show email items in a single card

--- src/DisplayEmailComponents/DisplayEmailCard/EmailCard.js
+++ src/DisplayEmailComponents/DisplayEmailCard/EmailCard.js
@@ -1,19 +1,19 @@
 import './DisplayEmailCards.css'
 
 const EmailCard = (props) => {
-    const emailItems = props.emailItems;
-    const emails = emailItems.map((emailItem) =>
-        <ul>{emailItem}</ul>
-    )
+    // const emailItems = props.emailItems;
+    // const emails = emailItems.map((emailItem) =>
+    //     <ul>{emailItem}</ul>
+    // )
     return (
         <div>
             <div className="displayNameAndDate">
-                <h2 className="contactName">Maria Smith</h2>
-                <p className="receivedEmailDate">23/07/2022</p>
+                <h2 className="contactName">{props.name}</h2>
+                <p className="receivedEmailDate">{props.date_created}</p>
             </div>
             <div className="displaySubjectAndContent">
-                <p className="emailSubject"></p>
-                <p className="emailContent"></p>
+                <p className="emailSubject">{props.subject}</p>
+                <p className="emailContent">{props.body}</p>
             </div>
         </div>
     )
--- src/DisplayEmailComponents/DisplayEmailContainer/DisplayEmailContainer.js
+++ src/DisplayEmailComponents/DisplayEmailContainer/DisplayEmailContainer.js
@@ -2,9 +2,15 @@ import './DisplayEmailContainer.css'
 import EmailCard  from "../DisplayEmailCard/EmailCard";
 
 const DisplayEmailContainer = () => {
+    const emailItems = props.emailItems;
+    const emails = emailItems.map((emailItem) =>
+        <EmailCard name={name} date_created={date_created}
+                   subject={subject} body={body} />
+    )
    return (
        <div className="DisplayEmailContainer col-3">
-           <EmailCard emailCard={EmailCard} />
+
+
        </div>
 
    )
@@ -12,3 +18,8 @@ const DisplayEmailContainer = () => {
 export default DisplayEmailContainer
 
 
+// const exampleName = 'Rayna'
+// const exampleDate = '2022-30-08'
+// const exampleSubject = 'Bills unpaid'
+// const exampleBody = 'Please pay the bills otherwise dire consequences will follol'
+// <EmailCard name={exampleName} date_created={exampleDate} subject={exampleSubject} body={exampleBody} />
