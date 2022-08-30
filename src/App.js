e7e772eec27a0587f27dfc0513f13a3e42fe6ff2
Harry <virtualslide@gmail.com>
2022-08-30 15:28:03 +0100

    merged sidebar, header and email container together

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import DisplayEmailContainer from "./DisplayEmailComponents/DisplayEmailContainer/DisplayEmailContainer";
 import Header from './Header/Header.js'
+import SideBar from './SideBar/SideBar.js'
 
 function App() {
   return (
     <div className="App">
-      <DisplayEmailContainer />
-      <Header />
+        <Header />
+        <div className={'d-flex flex-row vh-100'}>
+            <SideBar />
+            <DisplayEmailContainer />
+        </div>
     </div>
-  );
+  )
 }
-
 export default App;
