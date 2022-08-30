7f3994295a08142dda0301af261996b85b4bf3bc
Josh Bennet <joshbennet0@gmail.com>
2022-08-30 10:33:08 +0100

    started Bootstrap and CSS

--- src/displayEmailSnippet/displayEmailContainer/DisplayEmailContainer.js
+++ src/displayEmailSnippet/displayEmailContainer/DisplayEmailContainer.js
@@ -1,7 +1,12 @@
+import './DisplayEmailContainer.css'
+
+
 function displayEmailContainer() {
     return (
-        <div className="displayEmailContainer">
-            
+        <div className="displayEmailContainer" ".col-3">
+
         </div>
     )
 }
+
+export default DisplayEmailContainer
