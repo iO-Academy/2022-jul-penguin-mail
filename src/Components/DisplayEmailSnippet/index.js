7c09a0da3ed5db0e3ac4009b49c7182c917e8323
Mike Oram <Mikeoram01@gmail.com>
2022-08-30 12:03:33 +0100

    updated the placehodler address to use the API address.

--- src/Components/DisplayEmailSnippet/index.js
+++ src/Components/DisplayEmailSnippet/index.js
@@ -4,10 +4,10 @@ const DisplayEmailSnippet = () => {
     const [allEmails, setAllEmails] = useState(null)
     
     const fetchAllEmailData = async () => {
-        const EmailData = await fetch('https://jsonplaceholder.typicode.com/posts')
+        const EmailData = await fetch('http://localhost:8080/emails')
         const jsonEmailData = await EmailData.json()
         await setAllEmails(jsonEmailData)
-
+        
     }
 
     useEffect(() => {
