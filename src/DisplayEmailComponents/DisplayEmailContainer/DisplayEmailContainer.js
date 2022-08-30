26bed42067da5fd6c1b995de820405edfcc6540d
Josh Bennet <joshbennet0@gmail.com>
2022-08-30 14:54:40 +0100

    Updated the EmailContainer

--- src/DisplayEmailComponents/DisplayEmailContainer/DisplayEmailContainer.js
+++ src/DisplayEmailComponents/DisplayEmailContainer/DisplayEmailContainer.js
@@ -1,43 +1,14 @@
 import './DisplayEmailContainer.css'
 import EmailCard  from "../DisplayEmailCard/EmailCard";
 
-// const DisplayEmailContainer = EmailCard.map((DisplayEmailCard) =>
-//     {
-//         const emailItem = <EmailCard emailCard={DisplayEmailCard}/>
-//             return (
-//                 <ul className="DisplayEmailContainer .col-3 ">
-//                     {emailItem}
-//                 </ul>
-//             )
-//     }
-// )
-//
-// export default DisplayEmailContainer
+const DisplayEmailContainer = () => {
+   return (
+       <div className="DisplayEmailContainer col-3">
+           <EmailCard emailCard={EmailCard} />
+       </div>
 
-
-
-const DisplayEmailContainer = (props) => {
-        const emailItems = props.emailItems;
-        const emails = emailItems.map((emailItem) =>
-            <li>{emailItem}</li>
-        )
-    return (
-        <ul>{emails}</ul>
-    )
+   )
 }
-
 export default DisplayEmailContainer
 
 
-// function NameList(props) {
-//     const myLists = props.myLists;
-//     const listItems = myLists.map((myList) =>
-//         <li>{myList}</li>
-//     );
-//     return (
-//         <div>
-//             <h2>React Map Example</h2>
-//             <ul>{listItems}</ul>
-//         </div>
-//     );
-// }
