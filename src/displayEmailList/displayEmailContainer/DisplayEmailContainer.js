b2c23037e8e54fdcd475c32942e15e58b1fea5a4
Josh Bennet <joshbennet0@gmail.com>
2022-08-30 13:44:58 +0100

    did function for emails in parent

--- src/displayEmailList/displayEmailContainer/DisplayEmailContainer.js
+++ src/displayEmailList/displayEmailContainer/DisplayEmailContainer.js
@@ -1,12 +1,23 @@
 import './DisplayEmailContainer.css'
 import displayEmailCard  from "../displayEmailCard/DisplayEmailCard";
 
-function displayEmailContainer() {
-    return (
-        <div className="displayEmailContainer .col-3 ">
-            <DisplayEmailCard emailCard={displayEmailCard} />
-        </div>
-    )
-}
+const emails = displayEmailContainer.map((displayEmailContainer) =>
+    {
+        const emailItem = <DisplayEmailCard emailCard={displayEmailCard}/>
+            return (
+                <ul className="displayEmailContainer .col-3 ">
+                    {emailItem}
+                </ul>
+            )
+    }
+)
 
 export default DisplayEmailContainer
+
+// function displayEmailContainer() {
+//     return (
+//         <div className="displayEmailContainer .col-3 ">
+//             <DisplayEmailCard emailCard={displayEmailCard} />
+//         </div>
+//     )
+// }
