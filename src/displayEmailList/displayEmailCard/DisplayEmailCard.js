435d826304beff31f1b41aa1e6520770b891ecba
Johanna <Johannaloe@gmail.com>
2022-08-30 11:58:28 +0100

    basic layout for DisplayEmailCard.js complete

--- src/displayEmailList/displayEmailCard/DisplayEmailCard.js
+++ src/displayEmailList/displayEmailCard/DisplayEmailCard.js
@@ -4,9 +4,14 @@ function displayEmailCard() {
 
     return (
         <div>
-            <h2 className="contactName">Maria Smith</h2>
-            <p className="emailSubject"></p>
-            <p className="emailContent"></p>
+            <div className="displayNameAndDate">
+                <h2 className="contactName">Maria Smith</h2>
+                <p className="receivedEmailDate">23/07/2022</p>
+            </div>
+           <div className="displaySubjectAndContent">
+               <p className="emailSubject"></p>
+               <p className="emailContent"></p>
+           </div>
         </div>
     )
 }
