1f40e9b0c824ab3bbe771379255e70dbc7bc428f
Johanna <Johannaloe@gmail.com>
2022-08-30 13:59:22 +0100

    merge conflicts resolved

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import './App.css';
-<<<<<<< HEAD
 import DisplayEmailContainer from "./DisplayEmailComponents/DisplayEmailContainer/DisplayEmailContainer";
-=======
 import Header from './Header/Header.js'
->>>>>>> 57be5da1f1508fcb21cbadbabf382bf942390533
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <DisplayEmailContainer />
-=======
       <Header />
->>>>>>> 57be5da1f1508fcb21cbadbabf382bf942390533
     </div>
   );
 }
