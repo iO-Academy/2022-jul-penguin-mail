dec4f6bb725b783616560c8bdee884bd4c84b0d4
Johanna <Johannaloe@gmail.com>
2022-08-30 12:17:17 +0100

    amended merge conflicts

--- src/displayEmailList/displayEmailContainer/DisplayEmailContainer.js
+++ src/displayEmailList/displayEmailContainer/DisplayEmailContainer.js
@@ -1,18 +1,10 @@
 import './DisplayEmailContainer.css'
 import displayEmailCard  from "../displayEmailCard/DisplayEmailCard";
-<<<<<<< HEAD
-=======
-import DisplayEmailCard from "../displayEmailCard/DisplayEmailCard";
-
->>>>>>> b04e60ee5c71dbc9eb6dd77ca94f067c53341939
 
 function displayEmailContainer() {
     return (
         <div className="displayEmailContainer" ".col-3">
-<<<<<<< HEAD
-=======
             <DisplayEmailCard emailCard={displayEmailCard} />
->>>>>>> b04e60ee5c71dbc9eb6dd77ca94f067c53341939
         </div>
     )
 }
