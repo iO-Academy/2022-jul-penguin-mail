4bb8bd1096d01f4748732eb538bda0f1343718b2
Nicolas Herbreteau <nicolasherbreteau@Nicolass-MacBook-Pro.local>
2022-08-30 15:24:04 +0100

    created fetch email by ID

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import './App.css';
 import SideBar from './SideBar'
+import {useState, useEffect} from "react";
 
 function App() {
+    const [emailSearchId, setEmailSearchId] = useState(98)
+    const [emailDataById, setEmailDataById] = useState([])
+    const [emailRepliesBoolean, setEmailRepliesBoolean] = useState(false)
+    const fetchEmailById = async () => {
+        let queryString = 'http://localhost:8080/emails/' + emailSearchId
+        if (emailRepliesBoolean) {
+            queryString = queryString + '?replies=1'
+        }
+        const EmailById = await fetch(queryString)
+        const jsonEmailById = await EmailById.json()
+        setEmailDataById(jsonEmailById)
+    }
+    useEffect(() => {
+        fetchEmailById()
+    }, [])
+    useEffect(() => {
+        console.log(emailDataById)
+    }, [emailDataById])
   return (
     <div className="App">
       <SideBar />
