55261b6b6bf0229a3473902f862849b21d505324
Harry <virtualslide@gmail.com>
2022-08-30 15:59:26 +0100

    styling changes to emailList

--- src/DisplayEmailComponents/DisplayEmailCard/EmailCard.js
+++ src/DisplayEmailComponents/DisplayEmailCard/EmailCard.js
@@ -1,16 +1,14 @@
-import './DisplayEmailCards.css'
-
 function EmailCard() {
 
     return (
         <div className={'border d-flex flex-row justify-content-between p-2'}>
            <div className="emailContainer">
-               <p>Maria Smith</p>
+               <p className={'font-weight-bold h5'}>Maria Smith</p>
                <p>EMAILSUBJECT</p>
                <p>body of the email</p>
            </div>
             <div className="dateContainer">
-                <p>23/07/2022</p>
+                <p className={'font-weight-bold'}>23/07/2022</p>
             </div>
         </div>
     )
--- src/DisplayEmailComponents/DisplayEmailContainer/DisplayEmailContainer.js
+++ src/DisplayEmailComponents/DisplayEmailContainer/DisplayEmailContainer.js
@@ -1,7 +1,16 @@
-import './DisplayEmailContainer.css'
 import EmailCard  from "../DisplayEmailCard/EmailCard";
+// import {useState} from 'react'
 
 const DisplayEmailContainer = () => {
+        // const [email, setEmail] = useState({
+        //         "id": "539",
+        //         "name": "Nickie Rusted",
+        //         "body": "this is the email body",
+        //         "email": "nrustedey@people.com.cn",
+        //         "subject": "SDA",
+        //         "date_created": "2022-06-30 18:01:08",
+        //         "read": "1"
+        // })
    return (
        <div className="DisplayEmailContainer col-4 overflow-auto">
            <EmailCard emailCard={EmailCard} />
@@ -18,9 +27,7 @@ const DisplayEmailContainer = () => {
            <EmailCard emailCard={EmailCard} />
            <EmailCard emailCard={EmailCard} />
            <EmailCard emailCard={EmailCard} />
-
        </div>
-
    )
 }
 export default DisplayEmailContainer
