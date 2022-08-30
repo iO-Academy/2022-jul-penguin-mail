b3507236c8943916cc085d24e7ba51a7ffa68195
Johanna <Johannaloe@gmail.com>
2022-08-30 11:38:52 +0100

    added incomplete function to DisplayEmailCard container

---
+++ src/displayEmailSnippet/displayEmailCard/DisplayEmailCard.js
@@ -0,0 +1,14 @@
+import './DisplayEmailCard.css'
+
+function displayEmailCard() {
+
+    return (
+        <div>
+            <h2 className="contactName">Maria Smith</h2>
+            <p className="emailSubject"></p>
+            <p className="emailContent"></p>
+        </div>
+    )
+}
+
+export default DisplayEmailCard
