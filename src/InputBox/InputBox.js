08162a9ae743814acab4c5c543d4d534fda8d217
tom <thomarhughes98@gmail.com>
2022-08-31 15:49:22 +0100

    form nearly there

--- src/InputBox/InputBox.js
+++ src/InputBox/InputBox.js
@@ -1,39 +1,44 @@
+import "./InputBox.css"
+
 const InputBox = () => {
     return (
-        <card class="col-lg-4">
+        <card id="inputBox" class="col-lg-6 border container bg-white pb-2 pl-3">
         <form>
             <div>
                 <input
+                    id="formBox"
                     type="email"
                     placeholder="To"
                     name="To"
-                    class="col-lg-12"
+                    class="col-lg-12 m-2"
                     required
                 />
             </div>
             <div>
                 <input
+                    id="formBox"
                     type="text"
                     placeholder="Subject"
                     name="Subject"
-                    class="col-lg-12"
+                    class="col-lg-12 m-2"
                     required
                 />
             </div>
             <div>
         <textarea
+            id="formBox"
             placeholder="Your message"
             name="message"
-            class="col-lg-12"
+            class="col-lg-12 m-2"
             required
         />
             </div>
-            <div>
-                <button type="submit" class="btn btn-secondary">
-                    Cancel
-                </button>
-                <button type="submit" class="btn btn-success">
+            <div id="formButton" class="col-lg-12">
+                <button type="submit" className="btn btn-success float-right ml-1">
                     Send
+                </button>
+                <button type="submit" class="btn btn-secondary float-right">
+                    Cancel
                 </button>
             </div>
         </form>
