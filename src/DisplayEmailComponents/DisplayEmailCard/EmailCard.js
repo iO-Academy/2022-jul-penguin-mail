17ec56fe71f2c030a9b16d3451a2fe1cfb6e35c5
Harry <virtualslide@gmail.com>
2022-08-31 11:04:01 +0100

    email cards now populate based on the fetch

--- src/DisplayEmailComponents/DisplayEmailCard/EmailCard.js
+++ src/DisplayEmailComponents/DisplayEmailCard/EmailCard.js
@@ -1,16 +1,29 @@
-function EmailCard() {
+function EmailCard(props) {
 
     return (
-        <div className={'border d-flex flex-row justify-content-between p-2'}>
-           <div className="emailContainer">
-               <p className={'font-weight-bold h5'}>Maria Smith</p>
-               <p>EMAILSUBJECT</p>
-               <p>body of the email</p>
-           </div>
+        <div className={'btn border d-flex flex-row justify-content-between p-2'}>
+            <div className="emailContainer">
+                <p className={'font-weight-bold h5'}>{props.name}</p>
+                <p>{props.subject}</p>
+                <p>{props.body}</p>
+            </div>
             <div className="dateContainer">
-                <p className={'font-weight-bold'}>23/07/2022</p>
+                <p className={'font-weight-bold'}>{props.date_created}</p>
             </div>
         </div>
     )
 }
+
 export default EmailCard
+
+// <div>
+// <div className="displayNameAndDate">
+//     <h2 className="contactName"></h2>
+// <p className="receivedEmailDate">{props.date_created}</p>
+// </div>
+// <div className="displaySubjectAndContent">
+//     <p className="emailSubject">{props.subject}</p>
+//     <p className="emailContent">{props.body}</p>
+// </div>
+// </div>
+// )
--- src/DisplayEmailComponents/DisplayEmailContainer/DisplayEmailContainer.js
+++ src/DisplayEmailComponents/DisplayEmailContainer/DisplayEmailContainer.js
@@ -1,25 +1,21 @@
-import EmailCard  from "../DisplayEmailCard/EmailCard";
+import EmailCard from "../DisplayEmailCard/EmailCard";
 
-const DisplayEmailContainer = () => {
-
-   return (
-       <div className="DisplayEmailContainer col-sm-12 col-md-4 overflow-auto">
-           <EmailCard emailCard={EmailCard} />
-           <EmailCard emailCard={EmailCard} />
-           <EmailCard emailCard={EmailCard} />
-           <EmailCard emailCard={EmailCard} />
-           <EmailCard emailCard={EmailCard} />
-           <EmailCard emailCard={EmailCard} />
-           <EmailCard emailCard={EmailCard} />
-           <EmailCard emailCard={EmailCard} />
-           <EmailCard emailCard={EmailCard} />
-           <EmailCard emailCard={EmailCard} />
-           <EmailCard emailCard={EmailCard} />
-           <EmailCard emailCard={EmailCard} />
-           <EmailCard emailCard={EmailCard} />
-           <EmailCard emailCard={EmailCard} />
-       </div>
-   )
+const DisplayEmailContainer = (props) => {
+    const dateConvert = (emailItem) => {
+        const headerDateUnix = Date.parse(emailItem.date_created)
+        const headerDate = new Date(headerDateUnix)
+        return headerDate.toLocaleDateString("en-GB")
+    }
+    const emailItems = props.allEmailSnippets;
+    const emailCardsArray = emailItems.map((emailItem) =>
+        <EmailCard name={emailItem.name} date_created={dateConvert(emailItem)}
+                   subject={emailItem.subject} body={emailItem.body}/>
+    )
+    return (
+        <div className="DisplayEmailContainer col-sm-12 col-md-4 overflow-auto">
+            {emailCardsArray}
+        </div>
+    )
 }
 export default DisplayEmailContainer
 
