d0c7d57dc629810c61fa79c31a1587f44597e25b
Nicolas Herbreteau <nicolasherbreteau@Nicolass-MacBook-Pro.local>
2022-08-31 11:32:38 +0100

    email body styling

--- src/ReadingPanel/EmailBody/index.js
+++ src/ReadingPanel/EmailBody/index.js
@@ -1,8 +1,10 @@
 const EmailBody = (props) => {
     const emailData = props.emailDataById
 
+
     return (
         <div>
+            <p className=" font-weight-bold h4">{emailData.subject}</p>
             <p>{emailData.body}</p>
 
         </div>
