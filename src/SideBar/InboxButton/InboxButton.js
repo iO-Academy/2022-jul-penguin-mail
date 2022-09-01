267640f709db621970c68dbf84fa277fadd7c9aa
Harry <virtualslide@gmail.com>
2022-09-01 10:17:07 +0100

    removed plural from id

--- src/SideBar/InboxButton/InboxButton.js
+++ src/SideBar/InboxButton/InboxButton.js
@@ -2,7 +2,7 @@ const InboxButton = (props) => {
     const unreadEmailCount = props.allEmailSnippets.filter((obj) => obj.read === 1).length;
     return (
         <div>
-            <button id="inboxButtons" type="button" className="btn btn-info w-100 text-left d-flex flex-row justify-content-between">Inbox
+            <button id="inboxButton" type="button" className="btn btn-info w-100 text-left d-flex flex-row justify-content-between">Inbox
                 <p className="bg-warning font-weight-bold text-dark pl-1 pr-1 rounded">{unreadEmailCount}</p>
             </button>
         </div>
