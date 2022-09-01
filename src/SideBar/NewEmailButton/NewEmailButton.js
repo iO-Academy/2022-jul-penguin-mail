011d60b19863c49ce01c4c94dce282e35cb412f4
Johanna <Johannaloe@gmail.com>
2022-09-01 14:59:13 +0100

    New email button css added

--- src/SideBar/NewEmailButton/NewEmailButton.js
+++ src/SideBar/NewEmailButton/NewEmailButton.js
@@ -1,7 +1,9 @@
+import './NewEmailButton.css'
+
 const NewEmailButton = () => {
     return (
         <div>
-            <button id="pageButtons" type="button" href="#" class="btn btn-info w-100 text-left btn-primary:success" >New Email</button>
+            <button id="newEmailButton" type="button" href="#" class="btn btn-info w-100 text-left btn-primary:success" >New Email</button>
         </div>
     )
 }
