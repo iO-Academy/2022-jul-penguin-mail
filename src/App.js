44dcba9fc80a98ed2c17765bf88137e84bdeac51
Harry <virtualslide@gmail.com>
2022-09-01 15:47:57 +0100

    fixed minor syntax errors

--- src/App.js
+++ src/App.js
@@ -8,19 +8,10 @@ import {useEffect, useState} from "react"
 
 const App = () => {
 
-    const dummyData =   {
-        "name": "Bob Ross",
-        "email": "bob.ross@paintings.com",
-        "subject": "Example",
-        "body": "Exmaple text",
-    }
-
     const [allEmailSnippets, setAllEmailSnippets] = useState([])
     const [sidebarIsHidden, setSidebarIsHidden] = useState(true)
     const [isSentButtonClicked, setIsSentButtonClicked] = useState(false)
     const [isInboxButtonClicked, setIsInboxButtonClicked] = useState(true)
-
-
 
     const fetchAllEmailData = async () => {
         const emailData = await fetch('http://localhost:8080/emails')
@@ -41,12 +32,14 @@ const App = () => {
     useEffect(() => {
         if(isSentButtonClicked === true){
             fetchAllSentEmails()
+            console.log('sent button clicked')
         }
     },[isSentButtonClicked])
 
     useEffect(() => {
         if(isInboxButtonClicked === true){
-            fetchAllSentEmails()
+            fetchAllEmailData()
+            console.log('inbox button clicked')
         }
     },[isInboxButtonClicked])
 
@@ -55,7 +48,7 @@ const App = () => {
         <Header setSidebarIsHidden={setSidebarIsHidden} sidebarIsHidden={sidebarIsHidden} />
         <main>
             <InputBox />
-            <SideBar sidebarIsHidden={sidebarIsHidden} allEmailSnippets={allEmailSnippets} isSentButtonClicked={isSentButtonClicked} setIsSentButtonClicked={setIsSentButtonClicked} isInboxButtonClicked={isSentButtonClicked} setIsInboxButtonClicked={setIsSentButtonClicked} />
+            <SideBar sidebarIsHidden={sidebarIsHidden} allEmailSnippets={allEmailSnippets} isSentButtonClicked={isSentButtonClicked} setIsSentButtonClicked={setIsSentButtonClicked} isInboxButtonClicked={isInboxButtonClicked} setIsInboxButtonClicked={setIsInboxButtonClicked} />
             <EmailCardList allEmailSnippets={allEmailSnippets} />
         </main>
     </div>
