7bbe0dc6fba03e0b519317e65ca5a8adaf99ce53
Harry <virtualslide@gmail.com>
2022-09-01 12:01:16 +0100

    restyled the menu button

--- src/Header/MenuButton/MenuButton.js
+++ src/Header/MenuButton/MenuButton.js
@@ -3,7 +3,7 @@ const MenuButton = (props) => {
         props.setSidebarIsHidden(!props.sidebarIsHidden)
     }
     return (
-            <button onClick={toggleMenu} className={'h4 my-auto border d-sm-block d-md-none'}>Menu</button>
+            <button onClick={toggleMenu} className={'btn btn-secondary mx-3 h4 my-auto border d-sm-block d-md-none'}>Menu</button>
     )
 }
 export default MenuButton
